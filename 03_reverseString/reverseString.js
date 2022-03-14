0bccb01cedea5b55e57d0ff8aebd6d165bfb46a1
jwagdav <jwagdav@gmail.com>
2022-03-13 21:31:58 -0700

    finished exercise three.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const splitString = string.split("");
+    const reverseArray = splitString.reverse();
+    
+    const newString = reverseArray.join("");
+    
+    return newString;
 };
+
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
