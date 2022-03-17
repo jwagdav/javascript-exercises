2f96285e9f3a4eef87161a5e955de6c630c6393e
jwagdav <jwagdav@gmail.com>
2022-03-16 22:42:38 -0700

    solved exercise 5 using Gauss equation

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    if(!Number.isInteger(x) || !Number.isInteger(y) || x < 0 || y < 0){
+        return "ERROR";
+    }
+    if(x < y){
+        return (y * (y + x)) / 2;
+    } else if ( x > y){
+        return (x * (x + y)) / 2;
+    }
+    
 };
 
 // Do not edit below this line
