81a54d01b100b0057877126729405e1715c09eb3
jwagdav <jwagdav@gmail.com>
2022-03-16 22:50:24 -0700

    solved exercise 6 using if statements and conditions

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    isLeapYear = false;
 
+    if(year % 4 === 0){
+        isLeapYear = true;
+    } 
+
+    if(year % 100 === 0) {
+        isLeapYear = false;
+    }
+
+    if(year % 100 === 0 && year % 400 === 0){
+        isLeapYear = true;
+    }
+
+    return isLeapYear;
 };
 
 // Do not edit below this line
