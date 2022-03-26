73fb48d055bb10d1694850a34ed56536bb536d1b
jwagdav <jwagdav@gmail.com>
2022-03-26 16:51:40 -0700

    finished exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.reduce((a, b) => {
+        const oldestAge = calculateAge(a.yearOfBirth, a.yearOfDeath);
+        const curAge = calculateAge(b.yearOfBirth, b.yearOfDeath);
+        return oldestAge > curAge ? a : b;
+    }) 
 };
 
+
+function calculateAge(birth, death) {
+    if(!death){
+        death = new Date().getFullYear();
+    } 
+
+    return death - birth;
+}
 // Do not edit below this line
 module.exports = findTheOldest;
