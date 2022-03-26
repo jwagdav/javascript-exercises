7841771e52c5bd3fc2893c0609d80edc864996c7
jwagdav <jwagdav@gmail.com>
2022-03-26 16:34:40 -0700

    finished exercise 10. I used a for loop to do it but I feel like there should be a math forumula to handle it.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+    if(x < 0){
+        return "OOPS";
+    } else if (x === 0){
+        return 0;
+    } else {
+        let a = 0;
+        let b = 1;
+        for(let i = 1; i < x; i++){
+            
+            const temp = b;
+            b = a + b;
+            a = temp;
+        }
 
+        return b;
+    }
 };
 
 // Do not edit below this line
